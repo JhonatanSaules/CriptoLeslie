4d8088bcfeea6a9d3a93cc6ec36df47b2f23107a
JhonatanSaules <jhonatansaules@hotmail.com>
2017-10-15 15:43:43 -0500

    Login

---
+++ criptoleslie/nube/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import include, url
+from . import views
+
+urlpatterns = [
+        # session
+        url(r'^login/$', views.LoginView.as_view(), name='login'),
+        url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
+        # register
+        url(r'^register/$', views.RegisterView.as_view(), name='register'),
+        url(r'^register/success/$',
+            views.RegisterSuccessView.as_view(
+            ), name='register-success'),
+    ]
